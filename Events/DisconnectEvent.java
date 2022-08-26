cdf9809ad1ea9f7fc27c6fca11ec442abddcf86d
fadu335 <106926707+fadu335@users.noreply.github.com>
2022-08-26 22:11:11 +0300

    Delete Events directory

--- Events/DisconnectEvent.java
+++
@@ -1,3 +0,0 @@
-package com.example.examplemod.Events;
-
-public class DisconnectEvent {}
--- Events/EntityCollisionEvent.java
+++
@@ -1,7 +0,0 @@
-package com.example.examplemod.Events;
-
-import net.minecraftforge.fml.common.eventhandler.Cancelable;
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-@Cancelable
-public class EntityCollisionEvent extends Event {}
